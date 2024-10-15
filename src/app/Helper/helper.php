9b04d1f26ef5dad9e7d3a297e6fe9adfef4bf8f0
dmitry.sh <amiddio.dev@gmail.com>
2024-10-15 17:02:29 +0400

    Added helper.php file with some functions

---
+++ src/app/Helper/helper.php
@@ -0,0 +1,58 @@
+<?php
+
+if (!function_exists('formatSize'))
+{
+    /**
+     * Функция переводит байты в килобайты, мегабайты и т.д.
+     *
+     * @param int $size
+     * @return string
+     */
+    function formatSize(int $size): string
+    {
+        $metrics[0] = 'байт';
+        $metrics[1] = 'Кб';
+        $metrics[2] = 'Мб';
+        $metrics[3] = 'Гб';
+        $metrics[4] = 'Тб';
+        $metric = 0;
+
+        while (floor($size / 1024) > 0) {
+            $metric++;
+            $size /= 1024;
+        }
+
+        return round($size, 1) . " " . ($metrics[$metric] ?? '???');
+    }
+}
+
+if (!function_exists('createAtomsFromPlainText'))
+{
+    /**
+     * Функция разбивает текст на слова атомы,
+     * и возвращает массив с кол-вом повторений
+     *
+     * @param string $text
+     * @return array
+     */
+    function createAtomsFromPlainText(string $text): array
+    {
+        $text = mb_strtolower($text);
+
+        $text = preg_replace('/\W+/u',' ', $text);
+        $text = preg_replace('/\s+/u',' ', $text);
+        $allWords = explode(' ', trim($text));
+
+        $uniqueWords = array_count_values($allWords);
+
+        $result = [];
+        foreach ($uniqueWords as $atom => $count) {
+            $result[] = ['atom' => strval($atom), 'cnt' => $count];
+        }
+
+        return [
+            'atoms' => $result,
+            'total_atoms' => array_sum($uniqueWords),
+        ];
+    }
+}
